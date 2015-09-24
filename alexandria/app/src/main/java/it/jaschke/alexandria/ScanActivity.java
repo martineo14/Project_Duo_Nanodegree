8f822469ecfb6ec1488dae1d1bc8437a22fa728a
Martin Pueyo <martinpueyo14@yahoo.com.ar>
2015-09-23 23:34:06 -0300

    Initial Commit

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/ScanActivity.java
@@ -0,0 +1,46 @@
+package it.jaschke.alexandria;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("SCAN", rawResult.getText()); // Prints scan results
+        Log.v("SCAN", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        Intent intent=new Intent();
+        intent.putExtra("SCAN_BAR_CODE", rawResult.getText());
+        setResult(1, intent);
+        finish();
+    }
+}
